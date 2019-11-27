75637010a33e05c4cb831e6183612d55abe44fee
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2019-11-26 21:59:47 -0300

    configurando nodemailer e mailtrap-envio de email de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  sevure: false,
+  auth: {
+    user: '32ae72086db614',
+    pass: 'c382cd10040ae6',
+  },
+  default: {
+    from: 'Team GoBarber <noreply@gobarber.com> ',
+  },
+};
